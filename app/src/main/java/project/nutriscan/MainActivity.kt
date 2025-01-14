74a3715482981335e1abe081fbc6e09e9fd7bcfe
Farhan Shaikh <blaxerf@gmail.com>
2025-01-14 14:28:04 +0530

    Added dependencies and initial test screen

--- app/src/main/java/project/nutriscan/MainActivity.kt
+++ app/src/main/java/project/nutriscan/MainActivity.kt
@@ -2,10 +2,20 @@ package project.nutriscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import project.nutriscan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ScanButton.setOnClickListener {
+            Toast.makeText(this,"Button is working",Toast.LENGTH_SHORT).show()
+        }
     }
 }
