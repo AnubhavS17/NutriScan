57718c14e7920f71e4363886e16fc52298912cd5
Farhan Shaikh <blaxerf@gmail.com>
2025-01-16 20:02:19 +0530

    Initial Commit

--- app/src/main/java/project/nutriscan/ui/HomeFragment.kt
+++ app/src/main/java/project/nutriscan/ui/HomeFragment.kt
@@ -1,15 +1,17 @@
 package project.nutriscan.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import project.nutriscan.R
 import project.nutriscan.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    private var _binding : FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,11 +21,23 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(layoutInflater)
 
         binding.scanBarcode.setOnClickListener {
-                Toast.makeText(activity, "Scan Button Tapped", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_HomeScreen_to_scanBarcodeFragment)
+            Toast.makeText(activity, "Scan Button Tapped", Toast.LENGTH_SHORT).show()
         }
 
-        binding.TypeBarcode.setOnClickListener {
-            Toast.makeText(activity, "Type Button Tapped", Toast.LENGTH_SHORT).show()
+
+        binding.SearchBarcode.setOnClickListener {
+            if (binding.editTextBarcode.text.isNotEmpty()){
+                if (binding.editTextBarcode.text.length == 16) {
+                    var input: String = binding.editTextBarcode.text.toString()
+                    Toast.makeText(activity, input, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity, "Barcode must be 16 digit", Toast.LENGTH_SHORT).show()
+                }
+            } else{
+                Toast.makeText(activity, "Please enter a barcode", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
 
