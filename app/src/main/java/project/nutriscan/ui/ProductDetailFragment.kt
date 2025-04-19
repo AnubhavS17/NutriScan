5ea986dbeeb720e07086b529dbfca2ae10c9cc3a
Farhan Shaikh <blaxerf@gmail.com>
2025-04-19 12:07:11 +0530

    Added More Mappings for Additives Tags and Converted ProductDetailFragment.kt layout from Linear to Constraint.

--- app/src/main/java/project/nutriscan/ui/ProductDetailFragment.kt
+++ app/src/main/java/project/nutriscan/ui/ProductDetailFragment.kt
@@ -18,12 +18,12 @@ import project.nutriscan.utils.UtilityFunctions.Companion.getAdditiveFullName
 import project.nutriscan.viewmodel.NutritionViewModel
 
 class ProductDetailFragment : Fragment() {
-    private var _binding : FragmentProductDetailBinding? = null
+    private var _binding: FragmentProductDetailBinding? = null
     private val binding get() = _binding!!
 
-   lateinit var viewmodel : NutritionViewModel
+    lateinit var viewmodel: NutritionViewModel
 
-   private val args : ProductDetailFragmentArgs by navArgs()
+    private val args: ProductDetailFragmentArgs by navArgs()
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -38,50 +38,66 @@ class ProductDetailFragment : Fragment() {
 
         //API Call to retrieve data.
         val barcode = args.barcode
-        viewmodel.searchProduct(barcode,"nutriments,allergens,image_url,additives_tags,ingredients_text_en")
+        viewmodel.searchProduct(
+            barcode,
+            "nutriments,allergens,image_url,additives_tags,ingredients_text_en"
+        )
 
         //Observing and Mapping Values.
         viewmodel.productDetails.observe(viewLifecycleOwner, Observer {
 
             //Nutriments
-            binding.carbs.text = "Carbs: " + (it.product?.nutriments?.carbohydrates?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.carbohydrates_unit ?: "")
+            binding.carbs.text =
+                "Carbs: " + (it.product?.nutriments?.carbohydrates?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.carbohydrates_unit ?: "")
 
-            binding.sugar.text = "Sugar: " + (it.product?.nutriments?.sugars?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.sugars_unit ?: "")
+            binding.sugar.text =
+                "Sugar: " + (it.product?.nutriments?.sugars?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.sugars_unit ?: "")
 
-            binding.energy.text = "Energy: " + (it.product?.nutriments?.energy?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.energy_unit ?: "")
+            binding.energy.text =
+                "Energy: " + (it.product?.nutriments?.energy?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.energy_unit ?: "")
 
             binding.fat.text = "Fat: " + (it.product?.nutriments?.fat?.toString() ?: "Not Found") +
                     (it.product?.nutriments?.fat_unit ?: "")
 
-            binding.fatPer100g.text = "Fat per 100g: " + (it.product?.nutriments?.fat_100g?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.fat_unit ?: "")
+            binding.fatPer100g.text =
+                "Fat per 100g: " + (it.product?.nutriments?.fat_100g?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.fat_unit ?: "")
 
-            binding.fiber.text = "Fiber: " + (it.product?.nutriments?.fiber?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.fiber_unit ?: "")
+            binding.fiber.text =
+                "Fiber: " + (it.product?.nutriments?.fiber?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.fiber_unit ?: "")
 
-            binding.nutritionalScore.text = "Nutritional Score: " + (it.product?.nutriments?.nutrition_score_fr?.toString() ?: "Not Found")
+            binding.nutritionalScore.text =
+                "Nutritional Score: " + (it.product?.nutriments?.nutrition_score_fr?.toString()
+                    ?: "Not Found")
 
-            binding.proteins.text = "Protein: " + (it.product?.nutriments?.proteins?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.proteins_unit ?: "")
+            binding.proteins.text =
+                "Protein: " + (it.product?.nutriments?.proteins?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.proteins_unit ?: "")
 
-            binding.salt.text = "Salt Content: " + (it.product?.nutriments?.salt?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.salt_unit ?: "")
+            binding.salt.text =
+                "Salt Content: " + (it.product?.nutriments?.salt?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.salt_unit ?: "")
 
-            binding.saturatedFat.text = "Saturated Fat: " + (it.product?.nutriments?.saturated_fat?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.saturated_fat_unit ?: "")
+            binding.saturatedFat.text =
+                "Saturated Fat: " + (it.product?.nutriments?.saturated_fat?.toString()
+                    ?: "Not Found") +
+                        (it.product?.nutriments?.saturated_fat_unit ?: "")
 
-            binding.sodium.text = "Sodium: " + (it.product?.nutriments?.sodium?.toString() ?: "Not Found") +
-                    (it.product?.nutriments?.sodium_unit ?: "")
+            binding.sodium.text =
+                "Sodium: " + (it.product?.nutriments?.sodium?.toString() ?: "Not Found") +
+                        (it.product?.nutriments?.sodium_unit ?: "")
 
 
-          /*  //ProductName
-            binding.productName.text = "Name: " + it.product?.product_name*/
+            /*  //ProductName
+              binding.productName.text = "Name: " + it.product?.product_name*/
 
             //Ingredients
-            binding.ingredientsInfo.text = "Contains: ${it.product?.ingredients_text_en ?: "No ingredients available"}"
+            binding.ingredientsInfo.text =
+                "Contains: ${it.product?.ingredients_text_en ?: "No ingredients available"}"
 
             //Allergens
             val formattedAllergens = formatAllergens(it.product?.allergens)
@@ -103,34 +119,43 @@ class ProductDetailFragment : Fragment() {
 
     }
 
-@SuppressLint("ResourceAsColor")
-private fun displayAdditivesTags(additivesTags: List<String>?) {
-    if (!additivesTags.isNullOrEmpty()) {
-        val formattedAdditives = additivesTags.map { tag ->
-            val convertedTag = convertTagFormat(tag) // Convert the tag format
-            val fullName = getAdditiveFullName(convertedTag) // Get the full name for the additive code
-            "$convertedTag - $fullName" // Format as "E330 - Zeeshan"
+    @SuppressLint("ResourceAsColor")
+    private fun displayAdditivesTags(additivesTags: List<String>?) {
+        if (!additivesTags.isNullOrEmpty()) {
+            val formattedAdditives = additivesTags.map { tag ->
+                val convertedTag = convertTagFormat(tag) // Convert the tag format
+                val fullName =
+                    getAdditiveFullName(convertedTag) // Get the full name for the additive code
+                "$convertedTag - $fullName" // Format as "E330 - Zeeshan"
 
-        }.joinToString("\n") // Join the list into a single string
-        binding.additives.text = "Additives:\n$formattedAdditives"
-        binding.additives.setTextColor(R.color.pastel_yellow)
-    } else {
-        binding.additives.text = "Additives not found."
+            }.joinToString("\n") // Join the list into a single string
+            binding.additives.text = "Additives:\n$formattedAdditives"
+            binding.additives.setTextColor(R.color.pastel_yellow)
+        } else {
+            binding.additives.text = "Additives not found."
+        }
+
     }
-
-}
 
     fun convertTagFormat(tag: String): String {
         // Remove the "en:" prefix and convert to uppercase
         return tag.replace("en:", "").uppercase()
     }
+
     fun formatAllergens(allergens: String?): String {
         if (allergens.isNullOrEmpty()) {
             return "No allergens available"
         }
 
         val allergenList = allergens.split(",").map { it.trim() }
-        return allergenList.mapIndexed { index, allergen -> "${index + 1}. ${allergen.replace("en:", "")}" }
+        return allergenList.mapIndexed { index, allergen ->
+            "${index + 1}. ${
+                allergen.replace(
+                    "en:",
+                    ""
+                )
+            }"
+        }
             .joinToString("\n")
     }
 
